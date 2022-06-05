d0fe22c220e4999713c700266310313d25c4d25d
Xiaodong Liu <xiaodongliu@Xiaodongs-MacBook-Pro.local>
2022-06-05 00:13:01 -0400

    deploy

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,16 +1,21 @@
-/** @format */
-import firebase from 'firebase/app';
-import 'firebase/auth';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = firebase.initializeApp({
-	apiKey: 'AIzaSyDGgMrmA6krlAj3i4jEyDPinLMiVA67iGk',
-	authDomain: 'acornus-admin.firebaseapp.com',
-	databaseURL: 'https://acornus-admin-default-rtdb.firebaseio.com',
-	projectId: 'acornus-admin',
-	storageBucket: 'acornus-admin.appspot.com',
-	messagingSenderId: '830271715719',
-	appId: '1:830271715719:web:8680902f7528a3ae4cbb22',
-	measurementId: 'G-N7HS1W6EJZ',
-});
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyD0lmsiqpX-ns8HFQ3_UqndMe4HaFaw_7o",
+	authDomain: "joyce-secret-base.firebaseapp.com",
+	projectId: "joyce-secret-base",
+	storageBucket: "joyce-secret-base.appspot.com",
+	messagingSenderId: "749305795342",
+	appId: "1:749305795342:web:e8c5662ce8b0938af47d5f",
+	measurementId: "G-DBHMYHV768"
+};
 
-export default firebaseConfig;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
