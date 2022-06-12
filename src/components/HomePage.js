ac71c520071e90367543f6f9bcdf063581bf0075
Xiaodong Liu <xiaodongliu@Xiaodongs-MacBook-Pro.local>
2022-06-12 09:12:34 -0400

    class-3

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,10 +12,10 @@ const HomePage = () => {
 				<h1>JOYCE is the best!</h1>
 				<h4>Riddle</h4>
 				<Container text>
-					<p>A bundle of Blubber</p>
-					<p>a sight for Landlubbers</p>
-					<p>I wave with a slap-happy Tail</p>
-					<p>A saltwater Fountain</p>
+					<p>A bundle of <span inline style={{ color: "red" }}>Blubber</span></p>
+					<p>a sight for <span inline style={{ color: "purple" }}>Landlubbers</span></p>
+					<p>I wave with a slap-happy <span inline style={{ color: "green" }}>Tail</span></p>
+					<p>A saltwater  <span inline style={{ color: "blue" }}>Fountain</span></p>
 					<p>just sprayin' and spoutin'</p>
 					<p>look out! Thar I blow! I'm a ...</p>
 				</Container>
