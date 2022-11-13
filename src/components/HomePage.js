9baa09100016355375a9f9af65f9ce3c42fb7686
Xiaodong Liu <xiaodongliu@Xiaodongs-MacBook-Pro.local>
2022-11-13 01:25:41 -0500

    updated

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,15 +3,17 @@
 import React, { useState } from 'react';
 import { Container, Button, Image, Embed } from 'semantic-ui-react';
 import myImage from '../images/answer.png';
+import whaleSong from '../sound/whaleSong.wav';
 
 const HomePage = () => {
 	const [showAnswer, setShowAnswer] = useState(false);
+	const [playSound, setPlaySound] = useState(false);
 	return (
-		<div>
+		<div >
 			<Container text>
 				<h1>Joyce!</h1>
 				<h4>Riddle</h4>
-				<Container text>
+				<Container text style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
 					<p>A bundle of <span style={{ color: "red" }}>Blubber</span></p>
 					<p>a sight for <span style={{ color: "orange" }}>Landlubbers</span></p>
 					<p>I wave with a slap-happy <span style={{ color: "yellow" }}>Tail</span></p>
@@ -19,7 +21,15 @@ const HomePage = () => {
 					<p>just sprayin' and spoutin'</p>
 					<p>look out! Thar I blow! I'm a ...</p>
 				</Container>
-				<Button onClick={() => setShowAnswer(true)} style={{ color: "violet", backgroundColor: "black" }}>Show me the answer</Button>
+				<Container>
+					<Button onClick={() => setPlaySound(true)} style={{ color: 'rgba(7, 255, 255)', backgroundColor: "black" }}>Give me a hint</Button>
+					<Button onClick={() => setShowAnswer(true)} style={{ color: "violet", backgroundColor: "black" }}>Show me the answer</Button>
+				</Container>
+				{playSound &&
+					<audio controls>
+						<source src={whaleSong} type="audio/wav" />
+						Your browser does not support the audio element.
+					</audio>}
 				{showAnswer && <Embed
 					id='mdCa20EKTcY'
 					placeholder={myImage}
